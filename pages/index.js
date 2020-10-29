05ec2555497f9577561bf93d5a683e9d57ab55ed
jose-donato <zmcdonato@gmail.com>
2020-10-29 04:52:50 +0000

    adding joining room

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,11 @@ import { generateName } from '../lib/utils'
 import { useToasts } from "react-toast-notifications"
 
 export default function Home() {
-    const { addToast } = useToasts()
+  const { addToast } = useToasts()
   const [submitting, setSubmitting] = useState()
+  const [joinSubmitting, setJoinSubmitting] = useState()
   const [slug, setSlug] = useState("")
-  const [wantsCustomSlug, setWantsCustomSlug] = useState(false)
+  const [joinSlug, setJoinSlug] = useState("")
   const { push } = useRouter()
 
   const createRoomHandler = async () => {
@@ -22,7 +23,7 @@ export default function Home() {
       setSubmitting(false)
       if (success) {
         addToast('Room available. Redirecting...', { appearance: 'success', autoDismiss: true })
-        setTimeout(()=> {push(`/room/${slug}`)}, 200)
+        setTimeout(() => { push(`/room/${slug}`) }, 200)
       }
       else alert("room already exists")
     } catch (err) {
@@ -30,6 +31,26 @@ export default function Home() {
       console.log(err)
     }
   }
+
+    const joinRoomHandler = async () => {
+    setJoinSubmitting(true)
+    if (slug === "") {
+      return;
+    }
+    try {
+      const success = await createRoom(slug, {})
+      setJoinSubmitting(false)
+      if (success) {
+        addToast('Room available. Redirecting...', { appearance: 'success', autoDismiss: true })
+        setTimeout(() => { push(`/room/${slug}`) }, 200)
+      }
+      else alert("room already exists")
+    } catch (err) {
+      setSubmitting(false)
+      console.log(err)
+    }
+  }
+
   useEffect(() => {
     setSlug(generateName())
   }, [])
@@ -37,24 +58,47 @@ export default function Home() {
   return (
     <Fragment>
       <p className="mb-4">Helper for your presentations. Create a room, share the url with the attendees and colab!</p>
-      <div className="flex flex-col p-10 rounded-3xl content-center bg-white">
-        <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-normal mb-2"
-                for="username"
-              >
-                Slug
+      <div className="flex flex-col p-10 rounded-3xl content-center bg-white divide-y">
+        <div className="mb-6">
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-normal mb-2"
+              for="username"
+            >
+              Slug
               </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                type="text"
-                onChange={(e) => setSlug(e.currentTarget.value)}
-                required
-                autofocus
-                value={slug}
-              />
-            </div>
-        <button className={`self-end ${submitting ? "bg-blue-200 cursor-not-allowed" : ""}`} onClick={createRoomHandler}>{submitting ? "Creating Room" : "Create Room"}</button>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              onChange={(e) => setSlug(e.currentTarget.value)}
+              required
+              autofocus
+              value={slug}
+            />
+          </div>
+          <button className={`self-end ${submitting ? "bg-blue-200 cursor-not-allowed" : ""}`} onClick={createRoomHandler}>{submitting ? "Creating Room" : "Create Room"}</button>
+        </div>
+        <div>
+          <div className="mb-4 mt-8">
+            <label
+              className="block text-gray-700 text-sm font-normal mb-2"
+              for="username"
+            >
+              Slug
+              </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              onChange={(e) => setJoinSlug(e.currentTarget.value)}
+              required
+              value={joinSlug}
+              placeholder="Room slug"
+            />
+          </div>
+          <button className={`self-end ${submitting ? "bg-blue-200 cursor-not-allowed" : ""}`} onClick={joinRoomHandler}>{joinSubmitting ? "Joining Room" : "Join Room"}</button>
+        </div>
+
+
       </div>
     </Fragment>
   )
